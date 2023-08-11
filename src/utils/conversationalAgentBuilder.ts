3bff70e0c29c65369392966a8bbfcb23cba6bbf6
Hardik Kapadia <hardikkapadia@Hardiks-MacBook-Pro.local>
2023-08-11 17:16:36 +0530

    strict instructions added

--- src/utils/conversationalAgentBuilder.ts
+++ src/utils/conversationalAgentBuilder.ts
@@ -55,7 +55,7 @@ Use this if you want to respond directly and conversationally to the human. Mark
 \`\`\`json
 {{{{
     "action": "Final Answer",
-    "action_input": string // You should put what you want to return to use here and make sure to use valid json newline characters.
+    "action_input": string // You should put what you want to return to use here and make sure to use valid json newline characters as a mandatory tranformation. Do not introduce new line characters outside the action_input
 }}}}
 \`\`\`
 
